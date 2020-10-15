8e3f917265c6bea23047bd217404f8953a846020
christina-edu <christina-edu@users.noreply.github.com>
2020-10-14 23:29:17 -0400

    local merge and repush models

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4cfcbb918ef892197030c64dc93e0d31.js"
+  "/precache-manifest.3b56a75ab23affb92619fe2220a887da.js"
 );
 
 workbox.clientsClaim();
