b154c6e49e5693b05a33b19a0076e866dcbab323
James Ravelle <jxrvll@gmail.com>
2020-10-16 17:46:47 -0400

    Added product details page data (#12)

--- client/src/components/pages/Product/Product.js
+++ client/src/components/pages/Product/Product.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import './style.css';
+
+import API from '../../../utils/API';
+
+
 
 function Product(){
 
   let { id } = useParams();
 
+  const [product, setProduct] = useState({})
+
+  const productData = "";
+
+  const getProductDatabyId = () => {
+    API.getProductsID(id)
+      .then(res => {
+        console.log(res.data)
+        setProduct(res.data)
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getProductDatabyId();
+  }, []);
+
+
   return (
     <div className="container">
-      <h1>Product</h1>
-      <p>ID: {id} </p>
+      <h1>{ product.name }</h1>
+      <p><em>ID #: {id}</em></p>
+      <hr />
+      <ul style={{textAlign:"left"}}>
+        <li>Description: { product.description }</li>
+        <li>Country: { product.country }</li>
+        <li>Grape Blend: { product.grape_blend }</li>
+        <li>Size: { product.size }</li>
+        <li>Price { product.price }</li>
+        <li>Type: { product.type1 }</li>
+        <li>Style: { product.type2 }</li>
+      </ul>
     </div>
   );
 }
