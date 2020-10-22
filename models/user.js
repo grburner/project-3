b7c9be0500bd1bd872c3ba7be9fb3963ee4af68f
christina-edu <christina-edu@users.noreply.github.com>
2020-10-22 18:59:06 -0400

    added orders to seed

--- models/user.js
+++ models/user.js
@@ -12,7 +12,45 @@ const userSchema = new Schema({
     required: true 
   },
   role: String,
-  password: String //bcrypt.String
+  password: String, //bcrypt.String
+  address_street1: String,
+  address_street2: String,
+  address_city: String,
+  address_state: String,
+  address_zip: String,
+  phone_number: String,
+
+  //retailer
+  company_name: String, 
+  ships_to: [String],
+  cc_processing: String, //bcrypt.String,
+  bio: String,
+
+  //consumer
+  birth_date: { 
+    type: Date,
+    validate: function(input) {
+      return typeof new Date(input) === 'date' && new Date(input) >= Date.now - (21*365*24*60*60*1000);
+    },
+    message: input => 'You must be 21 to register!'
+  },
+  cc_on_file: [{
+    cc_number: String, //bcrypt.String,
+    expiration_month: Number,
+    expiration_year: Number
+  }],
+  favorites: [{
+    product_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  }]
+
+
+
+
+
+
 });
 
 const User = mongoose.model('User', userSchema);
