324bc73d73fd5a9720b2ab0251714221de4de7a2
Gary Burner <grburner@gmail.com>
2020-10-23 21:17:04 -0400

    adding ability to change price values in the products table

--- client/src/components/organisms/ProductsTable/index.js
+++ client/src/components/organisms/ProductsTable/index.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Table from 'react-bootstrap/Table';
 import API from '../../../utils/API';
+import Editable from '../../../utils/Editable.js';
 
 const ProductsTable = () => {
   const [chartData, setChartData] = useState([]);
   const [retailerId, setRetailerId] = useState("5f90df97d56aef06bcb010d3")
+  const [inputData, setInputData] = useState('')
+  const inputRef = useRef();
 
 
   useEffect(() => {
@@ -17,6 +20,7 @@ const ProductsTable = () => {
   const changeState = (field, index, value) => {
     const newData = chartData.map((d, i) => {
       if (i === index) {
+        console.log('into equal index')
         d[field] = value;
       }
       return d;
@@ -34,13 +38,44 @@ const ProductsTable = () => {
       }
     })
   }
+
+  const changeInput = e => {
+    console.log(e.target)
+    const id = e.target.dataset.id
+    const value = e.target.value
+    const index = e.target.dataset.index
+    chartData.forEach((elem, index) => {
+      if (id === elem._id) {
+        changeState('price', index , value)
+      }
+    })
+  }
   
   const renderProductRow = (element, index) => {
     return (
       <tr key={index} data-id={element._id}>
         <td  data-id={element._id} onClick={() => console.log('product changes saved')}>@</td>
         <td data-id={element._id} >{element.name}</td>
-        <td data-id={element._id} >{element.price}</td>
+        <td data-id={element._id}>
+          <Editable
+              text={inputData}
+              placeholder={element.price}
+              childRef={inputRef}
+              type='input'
+            >
+              <input 
+                ref={inputRef}
+                data-value={element.price} 
+                data-index={index}
+                data-id={element._id}
+                type='text'
+                //name='inputData'
+                placeholder={element.price}
+                value={element.price}
+                onChange={e => changeInput(e)}
+              />
+          </Editable>
+        </td>
         <td data-id={element._id} >{element.units}</td>
         <td data-index={index} data-id={element._id}  data-value={element.status} onClick={toggleStatus}>{element.status ? "Live" : "Paused"}</td>
       </tr>
