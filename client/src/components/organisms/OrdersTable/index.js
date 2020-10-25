134efdf949650f09b44089b6e163c1019517514f
Gary Burner <grburner@gmail.com>
2020-10-25 17:35:18 -0400

    cleaned up formatting and added status buttons on the orders table

--- client/src/components/organisms/OrdersTable/index.js
+++ client/src/components/organisms/OrdersTable/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { store } from '../../../utils/GlobalRetailerState';
 import OrderDetail from '../../organisms/OrderDetail';
 import DateFormatter from '../../../utils/DateFormatter';
+import Badge from 'react-bootstrap/Badge';
 
 import Table from 'react-bootstrap/Table';
 import API from '../../../utils/API';
@@ -52,20 +53,23 @@ const OrdersTable = () => {
   };
   
   const renderProductRow = (element, index) => {
-    // let date = new Date(element.date)
-    // let year = date.getFullYear();
-    // let month = date.getMonth()+1;
-    // let dt = date.getDate();
+    const orderDate = DateFormatter(element.date)
+    const sendDate = DateFormatter(element.date, 14)
 
     return (
       <tr key={index}>
-        <td onClick={e => passProdDetail(e)} data-index={index}>@</td>
+        <td onClick={e => passProdDetail(e)} data-index={index}>
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-receipt-cutoff" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v13h-1V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51L2 2.118V15H1V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zM0 15.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
+            <path fill-rule="evenodd" d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-8a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </td>
         <td>{element._id}</td>
         <td>{element.user_id}</td>
-        <td>{DateFormatter(element.date)}</td>
-        <td>{DateFormatter(element.date, 14)}</td>
+        <td>{orderDate}</td>
+        <td>{sendDate}</td>
         <td>{getTotal(element)}</td>
-        <td>{element.status}</td>
+        <td><Badge pill variant={(element.status == "open" ? "danger" : "success")}>{element.status}</Badge></td>
       </tr>
     );
   };
@@ -76,7 +80,7 @@ const OrdersTable = () => {
         <caption>Products</caption>
         <thead>
           <tr>
-            <th>@</th>
+            <th></th>
             <th>Order ID</th>
             <th>User Id</th>
             <th>Order Date</th>
--- client/src/utils/DateFormatter.js
+++ client/src/utils/DateFormatter.js
@@ -3,10 +3,8 @@ import React from 'react';
 const DateFomatter = (in_date, daysDelta = 0) => {
     const copy = new Date(in_date)
     const deltaDate = copy.setDate(copy.getDate() + daysDelta)
-    console.log(deltaDate)
     const date = new Date(deltaDate)
     const formattedDate = `${date.getMonth()} - ${date.getDate()} - ${date.getFullYear()}`
-    console.log(formattedDate)
 
     return formattedDate
 }
