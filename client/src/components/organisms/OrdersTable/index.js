735c50f116c1e7a898bde405486d7ca267bd575b
Gary Burner <grburner@gmail.com>
2020-10-25 23:02:58 -0400

    fixing spacing and header font sizes

--- client/src/components/organisms/OrdersTable/index.js
+++ client/src/components/organisms/OrdersTable/index.js
@@ -7,6 +7,8 @@ import Badge from 'react-bootstrap/Badge';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import API from '../../../utils/API';
 
 const OrdersTable = () => {
@@ -17,10 +19,10 @@ const OrdersTable = () => {
   const [detailIndex, setDetailIndex] = useState(0)
 
   useEffect(() => {
-    API.findByRetailerId(retailerId).then(data => {
-      setChartData(data.data);
-    });
-  }, []);
+    API.findByRetailerId(retailerId)
+      .then(data => {
+        setChartData(data.data)})
+    }, []);
 
   const getOrderInfo = (elem) => {
     let total = 0; 
@@ -33,8 +35,9 @@ const OrdersTable = () => {
     return {"total": total, "items": items};
   };
 
-  const passProdDetail = (e) => {
-    let index = e.target.dataset.index;
+  const passProdDetail = (ind = 0) => {
+    console.log('into prod')
+    let index = ind
     let passObj = {};
     let details = [];
 
@@ -62,10 +65,10 @@ const OrdersTable = () => {
 
     return (
         <tr key={index}>
-          <td onClick={e => passProdDetail(e)} data-index={index}>
-            <svg onClick={e => passProdDetail(e)} data-index={index} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-receipt-cutoff" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path onClick={e => passProdDetail(e)} data-index={index} fillRule="evenodd" d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v13h-1V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51L2 2.118V15H1V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zM0 15.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
-              <path onClick={e => passProdDetail(e)} data-index={index} fillRule="evenodd" d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-8a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
+          <td onClick={e => passProdDetail(e.target.dataset.index)} data-index={index}>
+            <svg onClick={e => passProdDetail(e.target.dataset.index)} data-index={index} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-receipt-cutoff" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path onClick={e => passProdDetail(e.target.dataset.index)} data-index={index} fillRule="evenodd" d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v13h-1V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51L2 2.118V15H1V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zM0 15.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
+              <path onClick={e => passProdDetail(e.target.dataset.index)} data-index={index} fillRule="evenodd" d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-8a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
             </svg>
           </td>
           <td>{'...' + element._id.substring(element._id.length - 10, element._id.length)}</td>
@@ -79,7 +82,15 @@ const OrdersTable = () => {
   };
 
   return (
-    <div>
+    <div className="mt-3">
+      <div>
+        <Navbar bg="light" expand="lg" className="w-100">
+          <Navbar.Brand href="#home"><h5>Your Orders</h5></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
       <Row className="mt-2">
         <Col sm={showDetail ? 8 : 12}>
           <Table>
@@ -110,7 +121,7 @@ const OrdersTable = () => {
 export default function OrderTable() {
 
   return (
-    <div className="App table-wrapper-scroll-y my-custom-scrollbar">
+    <div className="App table-wrapper-scroll-y my-custom-scrollbar w-100 mb-3">
       <OrdersTable />
     </div>
   );
--- client/src/components/organisms/RetailNav/index.js
+++ client/src/components/organisms/RetailNav/index.js
@@ -12,7 +12,7 @@ const RetailNav = () => {
 
   return (
     <Navbar bg="light" expand="lg" className="w-100">
-      <Navbar.Brand href="#home">Retail Portal</Navbar.Brand>
+      <Navbar.Brand href="#home"><h3>Retail Portal</h3></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
