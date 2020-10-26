5460836109f25bd6dfc5c7d83bf4c8b21368d81e
Gary Burner <grburner@gmail.com>
2020-10-25 21:16:47 -0400

    changed useEffect to render on data (#35)

--- client/src/components/organisms/OrderDetail/index.js
+++ client/src/components/organisms/OrderDetail/index.js
@@ -1,5 +1,78 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
+// import Table from 'react-bootstrap/Table';
+// import API from '../../../utils/API';
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+// import DateFormatter from '../../../utils/DateFormatter';
+// import Card from 'react-bootstrap/Card';
+// import Badge from 'react-bootstrap/Badge';
+// import Button from '../../atoms/Button/Button';
+
+// const OrderDetail = (props) => {
+//   const { data } = props;
+//   const [passedData, setPassedData] = useState(data)
+//   const [custName, setCustName] = useState('');
+//   const [prodNames, setProdNames] = useState([]);
+
+//   const getUserName = (id) => {
+//     API.getUserName(id).then((data) => {setCustName(data.data.name)})
+//   }
+
+//   useEffect(() => {
+//     let promises = [];
+//     const userName = getUserName(data.custId)
+//     const prodsList = data.products.forEach((elem) => {
+//       promises.push(
+//         API.getProductsID(elem.name)
+//           .then(data => {return data.data.name})
+//         );
+//     });
+//     Promise.all(promises).then((values) => {setProdNames(values)})
+//   }, [passedData]);
+
+//   const renderProductRow = (element, index) => {
+//     return (
+//       <tr key={index}>
+//         <td>{prodNames[index] ? prodNames[index] : 'no data'}</td>
+//         <td>{element.quantity}</td>
+//         <td>{element.cost}</td>
+//       </tr>
+//     );
+//   };
+//   return (
+
+//     <Card>
+//       <Card.Header>
+//         <Row  className="align-items-center">
+//           <Col>{custName ? custName : ''}</Col>
+//           <Col>{DateFormatter(data.orderDate)}</Col>
+//           <Col><Badge pill variant={(data.status == "open" ? "danger" : "success")}>{data.status}</Badge></Col>
+//         </Row>
+//       </Card.Header>
+//       <Card.Body>
+//         <Table>
+//           <tbody>
+//             {data.products.map(renderProductRow)}
+//           </tbody>
+//         </Table>
+//         <Row className="border-top border-dark">
+//           <Col className="mt-2">
+//             <Row className="d-flex justify-content-center">Ship By:</Row>
+//             <Row className="d-flex justify-content-center">{data.shipByDate}</Row>
+//           </Col>
+//           <Col className="mt-2">
+//             <Button onClick={() => console.log('ship me clicked')}>Ship Now</Button>
+//           </Col>
+//         </Row>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
+
+// export default OrderDetail;
+
+import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import API from '../../../utils/API';
 import Row from 'react-bootstrap/Row'
@@ -15,14 +88,18 @@ const OrderDetail = (props) => {
   const [custName, setCustName] = useState('');
   const [prodNames, setProdNames] = useState([]);
 
+  // useEffect(() => {
+  //   setPassedData(data)
+  // }, [])
+  console.log(data)
+
   const getUserName = (id) => {
     API.getUserName(id).then((data) => {setCustName(data.data.name)})
   }
-
   useEffect(() => {
     let promises = [];
+    getUserName(data.custId)
 
-    const userName = getUserName(data.custId)
     const prodsList = data.products.forEach((elem) => {
       promises.push(
         API.getProductsID(elem.name)
@@ -30,7 +107,7 @@ const OrderDetail = (props) => {
         );
     });
     Promise.all(promises).then((values) => {setProdNames(values)})
-  }, [passedData]);
+  }, [data]);
 
   const renderProductRow = (element, index) => {
     return (
@@ -41,8 +118,8 @@ const OrderDetail = (props) => {
       </tr>
     );
   };
-  return (
 
+  return (
     <Card>
       <Card.Header>
         <Row  className="align-items-center">
@@ -70,5 +147,4 @@ const OrderDetail = (props) => {
     </Card>
   );
 };
-
 export default OrderDetail;
