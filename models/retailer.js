b6a9a24f65830ffd89c2553b0ac0dda1d2767370
Christina Bayley (education) <christinabayley@comcast.net>
2020-10-27 19:52:15 -0400

    commented out js files to be deleted (#39)
    Co-authored-by: christina-edu <christina-edu@users.noreply.github.com>

--- models/retailer.js
+++ models/retailer.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-//const bcrypt = require("bcryptjs");
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// //const bcrypt = require("bcryptjs");
 
-const retailerSchema = new Schema({
-  user_id: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'User',
-    required: true 
-  },
-  name: { 
-    type: String, 
-    required: true 
-  },
-  address_street1: String,
-  address_street2: String,
-  address_city: String,
-  address_state: String,
-  address_zip: String,
-  ships_to: [String],
-  cc_processing: String, //bcrypt.String,
-  bio: String,
-  // products: [{
-  //   product_id: {
-  //     type: Schema.Types.ObjectId,
-  //     ref: 'Product'
-  //   },
-  //   additional_desc: String,
-  //   units: Number,
-  //   price: Number
-  //   //, image: mime
-  // }]
-});
+// const retailerSchema = new Schema({
+//   user_id: { 
+//     type: Schema.Types.ObjectId, 
+//     ref: 'User',
+//     required: true 
+//   },
+//   name: { 
+//     type: String, 
+//     required: true 
+//   },
+//   address_street1: String,
+//   address_street2: String,
+//   address_city: String,
+//   address_state: String,
+//   address_zip: String,
+//   ships_to: [String],
+//   cc_processing: String, //bcrypt.String,
+//   bio: String,
+//   // products: [{
+//   //   product_id: {
+//   //     type: Schema.Types.ObjectId,
+//   //     ref: 'Product'
+//   //   },
+//   //   additional_desc: String,
+//   //   units: Number,
+//   //   price: Number
+//   //   //, image: mime
+//   // }]
+// });
 
-const Retailer = mongoose.model('Retailer', retailerSchema);
+// const Retailer = mongoose.model('Retailer', retailerSchema);
 
-module.exports = Retailer;
+// module.exports = Retailer;
