235bf4de78e5ae8774469660e3838be43cd40e08
Gary Burner <grburner@gmail.com>
2020-11-16 09:00:51 -0500

    fixed navigation with header to marketplace

--- client/src/components/atoms/Headline/Headline.js
+++ client/src/components/atoms/Headline/Headline.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './style.css';
+import { useHistory } from 'react-router-dom';
 
 function Headline(){
+  let history = useHistory();
+
+  const handleClick = () => {
+    history.push('/');
+  }
+  
   return (
-    <h1 className="headline"><a href="/">WineHub</a></h1>
+    <h1 className="headline">
+      <a onClick={handleClick}>WineHub</a>
+    </h1>
   );
 }
   
